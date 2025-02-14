91a59b9dd749169dae2cc8871cdc1172c30554c7
programer12345anwar <mdanwar40212@gmail.com>
2025-02-15 02:03:30 +0530

    initial commit

---
+++ db-api/db-api/src/test/java/com/db/zepto/db_api/DbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.zepto.db_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
