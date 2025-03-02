4c60f78b394091a1c8693b5c0bdb80ca5a3660be
programer12345anwar <mdanwar40212@gmail.com>
2025-03-02 19:45:15 +0530

    created controller to handle db

--- central-api/central-api/src/main/java/com/central/zepto/central_api/util/DatabaseAPI.java
+++ central-api/central-api/src/main/java/com/central/zepto/central_api/util/DatabaseAPI.java
@@ -1,10 +1,23 @@
 package com.central.zepto.central_api.util;
 
+import com.central.zepto.central_api.model.AppUser;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
 import java.util.HashMap;
 
 public class DatabaseAPI {
     String dbApiUrl="/api/v1/db";
-    public void callCreateUserEndpoint(){
-
+    public void callCreateUserEndpoint(AppUser user){
+        String url="http://localhost:8081"+dbApiUrl+"user/save";
+        URI finalUrl=URI.create(url);
+        //create request entity
+        RequestEntity request=RequestEntity.post(finalUrl).body(user);
+        //create rest template
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<AppUser> response=restTemplate.exchange(url, HttpMethod.POST,request,AppUser.class);
     }
 }
