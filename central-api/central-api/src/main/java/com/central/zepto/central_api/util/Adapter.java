bcadae9a4ed753fcf60a70ffb24eec08fae2b661
programer12345anwar <mdanwar40212@gmail.com>
2025-03-18 01:03:16 +0530

    update in add user

--- central-api/central-api/src/main/java/com/central/zepto/central_api/util/Adapter.java
+++ central-api/central-api/src/main/java/com/central/zepto/central_api/util/Adapter.java
@@ -8,15 +8,16 @@ import org.springframework.stereotype.Component;
 @Component //it will create a new instance of this class
 public class Adapter {
     public AppUser mapUserRequestBodyToAppUser(RegisterUserDTO user){
-        AppUser appUser=AppUser.builder()
-                .name(user.getName())
-                .email(user.getEmail())
-                .address(user.getAddress())
-                .pincode(user.getPincode())
-                .status("ACTIVE")
-                .password(user.getPassword())
-                .phoneNumber(user.getPhoneNumber())
-                .build();
-        return appUser;
+        AppUser appUser = AppUser.builder()
+        .name(user.getName())
+        .userType(user.getUserType().toString())
+        .email(user.getEmail())
+        .address(user.getAddress())
+        .pincode(user.getPincode())
+        .status("ACTIVE")
+        .password(user.getPassword())
+        .phoneNumber(user.getPhoneNumber())
+        .build();
+    return appUser;
     }
 }
